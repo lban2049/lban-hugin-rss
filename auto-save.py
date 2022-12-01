02fbfb6aff54148a1298d2b3f0a150dabc3dd9ec
LBan <hanxl5123@163.com>
2022-12-01 11:28:59 +0800

    auto update

---
+++ auto-save.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+def urldownload(url,filename=None):
+    """
+    下载文件到指定目录
+    :param url: 文件下载的url
+    :param filename: 要存放的目录及文件名，例如：./test.xls
+    :return:
+    """
+    down_res = requests.get(url)
+    with open(filename,'wb') as file:
+        file.write(down_res.content)
+
+
+save_list = [
+    'http://localhost:3000/users/1/web_requests/9/github-trending-rss.xml'
+]
+
+# 下载列表中的rss文件
+for url in save_list:
+    filename = url.split('/')[-1]
+    urldownload(url, 'rss/' + filename)
+    print(filename + ' download succuess!')
+
+# 提交 git
+os.system("git add .")
+os.system("git commit -m 'auto update'")
+os.system("git push")
+
+print('auto push success')
